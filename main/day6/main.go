a8ec35c2e99604f610301dacdaf41b09eea83c6a
Alex <45183011+alexadams101@users.noreply.github.com>
2022-12-09 15:16:03 +0000

    Day 6/7

--- main/day6/main.go
+++ main/day6/main.go
@@ -1,6 +1,47 @@
 package main
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("hello world")
+	//Part 1
+	text := loadData("main/day6/input.txt")
+	var output int
+	for n:=0; n<len(text)-4; n++{
+		marker := text[n:n+4]
+		if !thereIsDuplicate(marker) {
+			output = n+4
+			break
+		}
+	}
+	fmt.Println("Part 1:", output)
+
+	//Part 2
+	var outputPt2 int
+	for n:=0; n<len(text)-14; n++{
+		marker := text[n:n+14]
+		if !thereIsDuplicate(marker) {
+			outputPt2 = n+14
+			break
+		}
+	}
+	fmt.Println("Part 2:", outputPt2)
 }
+
+func loadData(path string) string {
+	file, _ := os.ReadFile(path)
+	return string(file)
+}
+
+func thereIsDuplicate(marker string) bool {
+	m := make(map[rune]int)
+	for _,letter := range marker {
+		m[letter] ++
+	}
+	if len(m)<len(marker) {
+		return true
+	}
+	return false
+}
+
--- main/day7/main.go
+++ main/day7/main.go
@@ -1,6 +1,102 @@
 package main
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+	"strconv"
+	"sort"
+)
+
+type File struct {
+	Type string
+	Parent *File
+	Children map[string]*File
+	Size int
+}
+func (file *File) set_parent_size() {
+	if file.Parent != nil {
+		file.Parent.Size += file.Size
+	}
+}
+func create_dir(parent *File) *File {
+	return &File{"DIRECTORY", parent, make(map[string]*File), 0}
+}
+func create_file(parent *File, size int) *File {
+	return &File{"FILE", parent, make(map[string]*File), size}
+}
 
 func main() {
-	fmt.Println("hello world")
+	//Part 1
+	files := loadData("main/day7/test_input.txt")
+	fileSizes := []int{}
+	for i:=len(files)-1; i>=0; i-- {
+		for _, file := range files[i]{
+			file.set_parent_size()
+		}
+	}
+	for _, row := range files {
+		for _, file :=range row {
+			if file.Size<=100000 && file.Type=="DIRECTORY" {
+				fileSizes = append(fileSizes,file.Size)
+			}
+		}
+	}
+	total := 0
+	for _, size := range fileSizes {
+		total+=size
+	}
+	fmt.Println("Part 1:", total)
+
+	deletableFiles := []int{}
+	deletableBar := 30000000-(70000000-files[0][0].Size)
+	for _, row := range files {
+		for _, file :=range row {
+			if file.Size>=deletableBar && file.Type=="DIRECTORY" {
+				deletableFiles = append(deletableFiles,file.Size)
+			}
+		}
+	}
+	sort.Sort(sort.IntSlice(deletableFiles))
+	fmt.Println("Part 2:", deletableFiles[0])
+}
+
+func loadData(path string) [][]*File {
+	file, _ := os.ReadFile(path)
+	files := make([][]*File,0)
+	content := strings.Split(string(file), "\n")
+	root := create_dir(nil)
+	currentDir := root
+	dirLevel := 0
+	files = append(files, []*File{root})
+	dirLevel++
+	for _, line := range content[1:] {
+		if line[0:] == "$ cd .." {
+			dirLevel --
+			currentDir = currentDir.Parent
+		} else if line[0:4] == "$ cd" {
+			dirLevel ++
+			dirName := line[5:]
+			currentDir = currentDir.Children[dirName]
+		} else if line[0:4] == "$ ls" {
+		} else if line[0:3] == "dir" {
+			if dirLevel>len(files)-1 {
+				files = append(files, []*File{})
+			}
+			dirName := line[4:]
+			dir := create_dir(currentDir)
+			currentDir.Children[dirName] = dir
+			files[dirLevel] = append(files[dirLevel], dir)
+		} else {
+			if dirLevel>len(files)-1 {
+				files = append(files, []*File{})
+			}
+			file := strings.Split(line, " ")
+			name := file[1]
+			size,_ := strconv.Atoi(file[0])
+			f := create_file(currentDir, size)
+			currentDir.Children[name] = f
+			files[dirLevel] = append(files[dirLevel], f)
+		}
+	}
+	return files
 }
