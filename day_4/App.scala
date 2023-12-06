0ee07e56e2c916b6ac6dd7634f9cabbef8d01e2d
Alex <45183011+alexadams101@users.noreply.github.com>
2023-12-06 23:53:53 +0000

    Day 4

--- day_4/App.scala
+++ day_4/App.scala
@@ -6,8 +6,44 @@ import scala.io.Source
 
 def loadData(filename: String): Iterator[String] = Source.fromFile(filename).getLines
 
+class Game(var opp: List[Int], var self: List[Int])
+
 def part1(lines: Iterator[String]): String = 
-    "Not Complete"
+    lines.map(toGame(_)).map(getMatchingValues(_)).map(toPower(_)).sum.toString()
     
 def part2(lines: Iterator[String]): String = 
-    "Not Complete"
+    val values = lines.map(toGame(_)).map(getMatchingValues(_)).toList
+    getTotalScratchCards(values).toString()
+
+def toGame(s: String): Game =
+    val vals = s.split(":")(1).split("\\|")
+    val opp = vals(0).drop(1).dropRight(1).split(" ").filter(_.nonEmpty).map(_.toInt).toList
+    val self = vals(1).drop(1).split(" ").filter(_.nonEmpty).map(_.toInt).toList
+    new Game(opp, self)
+
+def getMatchingValues(g: Game): List[Int] =
+    g.opp.intersect(g.self)
+
+def toPower(i: List[Int]): Int = 
+    if (i.size > 0) 
+        Math.pow(2, i.size-1).toInt
+    else
+        0
+    
+def getTotalScratchCards(i: List[List[Int]]): Int = 
+    var counts = Array.fill(i.size)(1)
+    val sizes = i.map(_.size)
+    var total = 0
+
+    for (n <- 0 until i.size) {
+        if (sizes(n)>0) {
+            val no = sizes(n) * counts(n)
+            total = total + no
+            val e = no / sizes(n)
+            for (l <- n+1 to n+sizes(n)) {
+                counts.update(l, counts(l) + e)
+            }
+        }
+    }
+    
+    counts.sum
