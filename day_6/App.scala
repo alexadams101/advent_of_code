7095bad7a5912f5d57861c264fea518ae7672e0c
Alex <45183011+alexadams101@users.noreply.github.com>
2023-12-12 22:55:02 +0000

    day6

--- day_6/App.scala
+++ day_6/App.scala
@@ -7,7 +7,43 @@ import scala.io.Source
 def loadData(filename: String): Iterator[String] = Source.fromFile(filename).getLines
 
 def part1(lines: Iterator[String]): String = 
-    "Not Complete"
+    val races = mapToRaces(lines)
+    races.map(calculateWaysToWin).product.toString()
     
 def part2(lines: Iterator[String]): String = 
-    "Not Complete"
+    val race = mapToRace(lines)
+    calculateWaysToWin(race).toString()
+
+def calculateWaysToWin(race: (Long, Long)): Int =
+    val duration = race._1
+    val record = race._2
+    var q = 0
+    for (i <- 0.toLong until duration) {
+        if (i * (duration-i) > record) {
+            q = q + 1
+        }
+    }
+    q
+
+def calculateWaysToWin(race: (Long, Long), range: Range): Int =
+    val duration = race._1
+    val record = race._2
+    var q = 0
+    for (i <- range) {
+        if (i * (duration-i) > record) {
+            q = q + 1
+        }
+    }
+    q
+
+def mapToRaces(lines: Iterator[String]): List[(Long, Long)] = 
+    val numbers = lines.map(_.split(" ").filter(_.nonEmpty).drop(1)).toList
+    var list = List[(Long, Long)]()
+    for (i <- 0 until numbers(0).length) {
+        list = list:+ (numbers(0)(i).toLong, numbers(1)(i).toLong)
+    }
+    list
+
+def mapToRace(lines: Iterator[String]): (Long, Long) = 
+    val numbers = lines.map(_.split(":")(1).replace(" ", "")).toList
+    (numbers(0).toLong, numbers(1).toLong)
